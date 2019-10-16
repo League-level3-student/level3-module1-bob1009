be200bd964c111120643733ab1b4e7a4e62c5eab
ilabstaff <ilabstaff@172.16.116.128>
2019-10-15 17:58:29 -0700

    Finished dynamic int array test and guest list

--- src/_01_IntroToArrayLists/_02_GuestBook.java
+++ src/_01_IntroToArrayLists/_02_GuestBook.java
@@ -1,6 +1,15 @@
 package _01_IntroToArrayLists;
 
-public class _02_GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class _02_GuestBook implements ActionListener{
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -9,7 +18,42 @@ public class _02_GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	JFrame frame=new JFrame();
+	JButton button=new JButton();
+	JButton button2=new JButton();
+	JPanel panel=new JPanel();
+	ArrayList <String>names=new ArrayList<String>();
+	public static void main(String[] args) {
+		_02_GuestBook book=new _02_GuestBook();
 	
-	
+		
+	}
+		_02_GuestBook(){
+		frame.setSize(500, 500);
+		panel.add(button);
+		panel.add(button2);
+		frame.add(panel);
+		button.setText("Add Guest");
+		button2.setText("View List");
+		frame.setVisible(true);
+		button.addActionListener((ActionListener) this);
+		button2.addActionListener((ActionListener) this);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource()==button) {
+			String namesave=JOptionPane.showInputDialog("What is the person's name that you want to put on the list");
+			names.add(namesave);
+		}
+		if (e.getSource()==button2) {
+			String blank="";
+			for (int i = 0; i <names.size(); i++) {
+				blank+="Guest #"+(i+1)+": "+names.get(i)+"\n";
+			}
+			JOptionPane.showMessageDialog(null,""+blank );
+		}
+	}
 	
 }
